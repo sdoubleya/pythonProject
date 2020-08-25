c5afd355a5f6680d7b56c7269c5d973f5ea42b3c
sdoubleya <asaverywhiteway@gmail.com>
2020-08-25 12:48:00 -0400

    fixed comments

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def main(msg):
-    // Another comment
-    // adding the second comment to this file
-    //adding a comment
+    # Another comment
+    # adding the second comment to this file
+    # adding a comment
     print(msg)
 
 main("Hello People")
